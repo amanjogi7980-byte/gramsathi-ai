74f520df378c85dd1baedd47ac0500699b0fa90e
amanjogi7980-byte <amanjogi7980@gmail.com>
2026-06-18 22:46:06 +0530

    change model

--- backend/chatbot.py
+++ backend/chatbot.py
@@ -7,8 +7,9 @@ load_dotenv()
 genai.configure(
     api_key=os.getenv("GEMINI_API_KEY")
 )
+
 model = genai.GenerativeModel(
-    "gemini-1.5-flash-latest"
+    "gemini-2.0-flash"
 )
 
 def get_chat_response(message):
